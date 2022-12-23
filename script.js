f33189f72560b58fe7cd146f29e1c434d458fe41
Stian <01stisor@gmail.com>
2022-12-23 17:00:27 +0100

    icons done (ish  :/)

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-var btn = document.getElementById("search-btn");
-btn.addEventListener("click", function () {
-    btn.style.backgroundColor = "rgb(0, 120, 215)";
-    btn.style.color = "white";
-});
-btn.addEventListener("blur", function () {
-    btn.style.backgroundColor = "white";
-    btn.style.color = "black";
-});
 // TODO add inverting of the search icon
 
+function search_btn() {
+  console.log("search button clicked");
+
+  //variables
+  const blue = "#1DA1F2";
+  var modal = document.getElementById("search-modal");
+  var search_btn = document.getElementById("search-btn");
+
+  search_btn.onclick = function () {
+    search_btn.style.backgroundColor = blue;
+    search_btn.style.color = "white";
+
+    // make modal appear from the bottom
+    modal.style.transform = "translateY(0px)";
+    modal.style.transition = ".2s";
+  };
+
+  search_btn.onblur = function () {
+    search_btn.style.backgroundColor = "white";
+    // blue variable
+    search_btn.style.color = "black";
+    
+    // make modal disappear from the bottom
+    modal.style.transform = "translateY(1000px)";
+    modal.style.transition = ".2s";
+   
+
+  };
+}
